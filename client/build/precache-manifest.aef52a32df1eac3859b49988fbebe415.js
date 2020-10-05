a007ab211773f2cc1ba5bc307abf47d083fe32cf
gowthamiallu5 <gowthami.allu@augmentolabs.com>
2020-10-05 19:07:38 +0530

    ga

---
+++ client/build/precache-manifest.aef52a32df1eac3859b49988fbebe415.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40d01227af943d0c7d02ef78a0dcd95b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9eb9802b6ce99720b74d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d634b92862562113eb0e",
+    "url": "/static/js/2.fe640a42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fe640a42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9eb9802b6ce99720b74d",
+    "url": "/static/js/main.0995f9bf.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
